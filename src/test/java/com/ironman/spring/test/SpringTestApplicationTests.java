f964db2f7e2c5fed1745035614585951997c27b6
DESKTOP-6KBPV3D\user <jon00141@naver.com>
2023-07-05 20:32:53 +0900

    [여진]  - 스프링 테스트 최초 커밋

---
+++ src/test/java/com/ironman/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironman.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
